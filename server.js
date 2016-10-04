04702faf41a3cac1e005a6963a60618bc45d4725
jainaman1398 <ajjain.aman13@gmail.com>
2016-10-05 00:01:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/C:\Users\lenovo\Desktop', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'aj.png'));
 });
 
 
