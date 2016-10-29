21a0b4f669860553cd0174b389e630ad10c41f57
jainaman1398 <ajjain.aman13@gmail.com>
2016-10-29 22:34:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,12 @@ console.log('Loaded!');
 var img=document.getElementById('aj2');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+20;
+    marginLeft=marginLeft+80;
     img.style.margintop=marginLeft+'px';
     
 }
 img.onclick=function()
 {
-    var interval=setInterval(moveRight,2);
+    var interval=setInterval(moveRight,1);
 };
 
