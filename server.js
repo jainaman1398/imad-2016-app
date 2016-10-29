2820916b4cf118bd19a70ae89937faeb6aed1df7
jainaman1398 <ajjain.aman13@gmail.com>
2016-10-29 22:58:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,9 +67,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-//app.get('/ui/IMG_20160709_124628.jpg', function (req, res) {
-//  res.sendFile(path.join(__dirname, 'ui', 'IMG_20160709_124628.jpg'));
-//});
+app.get('/ui/IMG_20160709_124628.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'IMG_20160709_124628.jpg'));
+});
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
