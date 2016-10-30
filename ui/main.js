e6158c7e5d0ce4a0f659a570352cf9e4fea36b93
jainaman1398 <ajjain.aman13@gmail.com>
2016-10-30 09:17:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,4 +11,12 @@ img.onclick=function()
 {
     var interval=setInterval(moveRight,1);
 };
+var counter=0;
+var button=document.getElementById("button");
+button.onclick=function(){
+    counter=counter+1;
 
+var span=document.getElementById("span");
+span.innerHtml=counter.toString();
+
+};
