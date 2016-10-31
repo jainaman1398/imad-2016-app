f14d01ea064f3d06cd4057b0d8674d477cb97482
jainaman1398 <ajjain.aman13@gmail.com>
2016-10-31 10:13:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config={
   password:process.env.DB_PASSWORD
     
 };
-var pool=new pool(config);
+var pool= new pool(config);
 app.get('/test-db',function(req,res){
     
     pool.query('SELECT * FROM article',function(err,result){
