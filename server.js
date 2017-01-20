6c47450cab2b105d998bae9d99fffd61fab791c8
jainaman1398 <ajjain.aman13@gmail.com>
2017-01-20 21:46:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config = {
     database: 'jainaman1398',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD
+    password: db-jainaman1398-57667
 };
 
 var app = express();
